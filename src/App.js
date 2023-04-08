f87d8da40199c332a684ffebce1175fd8223ce2a
Jahnavi Lenka <jahnavi2003lenka@gmail.com>
2023-04-09 00:19:17 +0530

    events added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { ReactDOM } from 'react'
+import './index.css'
+import Data from './Data'
 
-const App = () => {
-  return (
-    <div>App</div>
+/*function  nCard(val) {
+  return(
+    <Card
+    img={val.img}
+    title={val.title}
+    />
+  )
+}*/
+
+function App() {
+  return(
+    <div>
+    <div className='headin_header'>
+    <h1 className='heading_style'>EVENTS</h1>
+    </div>
+  <div className='container'>
+        {Data.map((item)=>{
+         return(
+        <div className='card'>
+        <div className='card-img'>
+        <img src={item.img} alt="pic" />
+        </div>
+         <div className='title'>
+         <h2>{item.title}</h2>
+        </div>
+        </div>
+         );
+        })}
+    </div>
+</div>
   )
 }
 
-export default App
+export default App;
---
+++ src/Data.jsx
@@ -0,0 +1,27 @@
+const Data=[
+    {
+        img:"./images/Tech.png",
+        title:"TECHNICAL EVENTS"
+    },
+    {
+        img:"./images/Lit.png",
+        title:"LITERARY EVENTS"
+    },
+    {
+        img:"./images/Manage.png",
+        title:"MANAGEMENT EVENTS"
+    },
+    {
+        img:"./images/Flag.png",
+        title:"FLAGSHIP EVENTS"
+    },
+    {
+        img:"./images/Flag.png",
+        title:"FUN EVENTS"
+    },
+    {
+        img:"./images/Flag.png",
+        title:"WORKSHOPS"
+    },
+]
+export default Data;
